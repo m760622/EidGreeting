814fb345aec058ec241634b7649847a8bd595312
Mohammed Abunada <m7606225@gmail.com>
2020-08-01 19:59:50 +0200

    Initial Commit

---
+++ EidGreeting/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  EidGreeting
+//
+//  Created by Mohammed Abunada on 2020-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EidGreeting/EidGreetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EidGreetingApp.swift
+//  EidGreeting
+//
+//  Created by Mohammed Abunada on 2020-08-01.
+//
+
+import SwiftUI
+
+@main
+struct EidGreetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
